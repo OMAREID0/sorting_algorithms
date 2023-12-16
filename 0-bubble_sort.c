bcb01c9eec0a3d23ae3aa3b110bdfe77d4ff6057
omar eid <omareid221b@gmail.com>
2023-12-16 21:56:26 +0200

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -19,7 +19,7 @@ void bubble_sort(int *array, size_t size)
     {
 	    for (j = 0; j < size - i - 1; j++)
 	    {
-		    if (array[j] > array[j + 1])
+		    if (array[i] > array[j + 1])
 		    {
 			    tmp = array[j + 1];
 			    array[j + 1] = array[j];
