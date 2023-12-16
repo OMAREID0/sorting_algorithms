bb8a83e9631d4f1bbd389143d7b79ee0a03d7994
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2023-12-16 22:55:18 +0200

    Update swap.c

--- swap.c
+++ swap.c
@@ -1,9 +1,15 @@
 #include "sort.h"
 
-
+/**
+ * swap - swaps two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: void
+ */
 void swap(int *a, int *b)
 {
     int temp;
+    
     temp = *a;
     *a = *b;
     *b = temp;
