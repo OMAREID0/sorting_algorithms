447881ffc612770d9d6cc3c401089c0a4353c99f
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2023-12-16 22:38:40 +0200

    Update swap.c

--- swap.c
+++ swap.c
@@ -1,22 +1,10 @@
 #include "sort.h"
 
 
-void bubble_sort(int *array, size_t size)
+void swap(int *a, int *b)
 {
-    int *p, *q;
-
-    if (array == NULL || size < 2)
-        return;
-
-    for (p = array; p < array + size - 1; p++)
-    {
-        for (q = array; q < array + size - 1; q++)
-        {
-            if (*q > *(q + 1))
-            {
-                swap(q, q + 1);
-            }
-        }
-    }
-    print_array(array, size);
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
 }
