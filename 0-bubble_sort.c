f5bc3ded7be58105f97aa8e0c8df68aacdcc9e15
urahara <omareid221b@gmail.com>
2023-12-16 21:54:21 +0200

    Bubble sorting

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -15,11 +15,11 @@ void bubble_sort(int *array, size_t size)
     if (array == NULL || size < 2)
         return;
 
-    for (; i < size - 1; i++)
+    for (i = 0; i < size - 1; i++)
     {
 	    for (j = 0; j < size - i - 1; j++)
 	    {
-		    if (array[j] > array[j + 1])
+		    if (array[i] > array[j + 1])
 		    {
 			    tmp = array[j + 1];
 			    array[j + 1] = array[j];
