d923947195433b806421d3ba8c8f42bccae532bc
OMAREID0 <omareid221b@gmail.com>
2023-12-19 00:05:15 +0200

    selection_sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,20 +9,26 @@
  * Return: void
  */
 void selection_sort(int *array, size_t size)
-{
-    size_t i, j, k;
+{   
+    size_t i, j, min;
+    int temp;
+
+    if (array == NULL || size < 2)
+        return;
 
     for (i = 0; i < size - 1; i++)
     {
-        k = i;
+        min = i;
         for (j = i + 1; j < size; j++)
         {
-            if (array[j] < array[k])
-                k = j;
+            if (array[j] < array[min])
+                min = j;
         }
-        if (k != i)
+        if (min != i)
         {
-            swap(&array[k], &array[i]);
+            temp = array[i];
+            array[i] = array[min];
+            array[min] = temp;
             print_array(array, size);
         }
     }
