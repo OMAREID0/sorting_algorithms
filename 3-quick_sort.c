843881486d9294c13f0c0858baf41b424baa0f86
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2023-12-19 15:45:26 +0200

    Update 3-quick_sort.c

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,78 +1,80 @@
 #include "sort.h"
 
 /**
- * swap - swaps two integers
- * @a: first integer
- * @b: second integer
- * Return: void
+ * swap - Swaps two integers.
+ * @a: First integer.
+ * @b: Second integer.
+ * Return: Void.
  */
 void swap(int *a, int *b)
 {
-        int temp;
+    int temp;
 
-        temp = *a;
-        *a = *b;
-        *b = temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
 /**
- * partitioner - slices the array into lesser and greater and pivot.
- * @arr: the array to be partitioned.
- * @low: the beginning of the array `arr`.
- * @high: the last element in the array `arr`.
- * @size: size of the array
+ * partitioner - Partitions the array into lesser and greater elements around the pivot.
+ * @arr: The array to be partitioned.
+ * @low: The beginning of the array.
+ * @high: The last element in the array.
+ * @size: Size of the array.
  *
- * Returns: the pivot index `i + 1`.
+ * Returns: The pivot index `i + 1`.
  */
 int partitioner(int *arr, int low, int high, size_t size)
 {
-        int i, j, pivot;
+    int i, j, pivot;
 
-        i = low - 1;
-        pivot = arr[high];
-        for (j = low; j <= high - 1; ++j)
+    i = low - 1;
+    pivot = arr[high];
+
+    for (j = low; j <= high - 1; ++j)
+    {
+        if (arr[j] < pivot)
         {
-                if (arr[j] < pivot)
-                {
-                        swap(&arr[j], &arr[++i]);
-			print_array(arr, size);
-                }
+            swap(&arr[j], &arr[++i]);
+            print_array(arr, size);
         }
-        swap(&arr[i + 1], &arr[high]);
-        print_array(arr, size);
-        return (i + 1);
+    }
+
+    swap(&arr[i + 1], &arr[high]);
+    print_array(arr, size);
+
+    return (i + 1);
 }
 
 /**
- * lomuto_scheme - Lomuto partition scheme for QuickSort
- * @arr: the array to be sorted
- * @low: the beginning of the array
- * @high: the last element in the array
- * @size: size of the array
+ * lomuto_scheme - Lomuto partition scheme for QuickSort.
+ * @arr: The array to be sorted.
+ * @low: The beginning of the array.
+ * @high: The last element in the array.
+ * @size: Size of the array.
  *
- * Returns: void
+ * Returns: Void.
  */
 void lomuto_scheme(int *arr, int low, int high, size_t size)
 {
-        int pi;
+    int pi;
 
-        if (low < high)
-        {
-                pi = partitioner(arr, low, high, size);
-                lomuto_scheme(arr, low, pi - 1, size);
-                lomuto_scheme(arr, pi + 1, high, size);
-        }
+    if (low < high)
+    {
+        pi = partitioner(arr, low, high, size);
+        lomuto_scheme(arr, low, pi - 1, size);
+        lomuto_scheme(arr, pi + 1, high, size);
+    }
 }
 
 /**
- * quick_sort - sorts an array of integers in ascending order.
- * using the Quick sort algorithm(recursively).
- * @array: the array to be sorted
- * @size: size of the array
+ * quick_sort - Sorts an array of integers in ascending order using the Quick sort algorithm.
+ * @array: The array to be sorted.
+ * @size: Size of the array.
  *
- * Returns: void
+ * Returns: Void.
  */
 void quick_sort(int *array, size_t size)
 {
-	lomuto_scheme(array, 0, size - 1, size);
+    lomuto_scheme(array, 0, size - 1, size);
 }
