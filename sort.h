a844ec4b0c98514134161e05dd1701a75a79264f
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2023-12-19 14:33:31 +0200

    Update sort.h

--- sort.h
+++ sort.h
@@ -16,7 +16,7 @@ typedef struct listint_s
     struct listint_s *prev;
     struct listint_s *next;
 } listint_t;
-
+int partitioner(int *arr, int low, int high);
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
@@ -24,4 +24,5 @@ void swap(int *a, int *b);
 void swapper(listint_t **list, listint_t *pre, listint_t *cur);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 #endif
