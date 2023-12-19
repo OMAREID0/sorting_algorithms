2284a5052a24ecb82cb4fd14746416fb9079cbcd
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2023-12-19 14:19:53 +0200

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,11 +7,11 @@
  */
 void swap(int *a, int *b)
 {
-        int temp;
+	int temp;
 
-        temp = *a;
-        *a = *b;
-        *b = temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
 }
 /**
  * bubble_sort - Sorts an array of integers in ascending order.
@@ -22,19 +22,19 @@ void swap(int *a, int *b)
  */
 void bubble_sort(int *array, size_t size)
 {
-        int *p, *q;
+	int *p, *q;
 
-        if (array == NULL || size < 2)
-        return;
-        for (p = array; p < array + size - 1; p++)
-        {
-                for (q = array; q < array + size - 1; q++)
-                {
-                        if (*q > *(q + 1))
-                        {
-                                swap(q, q + 1);
-                                print_array(array, size);
-                        }
-                }
-        }
+	if (array == NULL || size < 2)
+	return;
+	for (p = array; p < array + size - 1; p++)
+	{
+		for (q = array; q < array + size - 1; q++)
+		{
+			if (*q > *(q + 1))
+			{
+				swap(q, q + 1);
+				print_array(array, size);
+			}
+		}
+	}
 }
